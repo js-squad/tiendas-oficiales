93edbe8c5f49fb08b1521d50d15adfd55bdabbf1
Santiago <smarchioni87@gmail.com>
2016-01-29 13:39:03 -0300

    Search API - grupo 4 -seguimos agregando valor-

--- components/api/index.js
+++ components/api/index.js
@@ -34,18 +34,60 @@
   // }
   // the values for the 'next' keys should be the same as the ones received
   // from the API except for the 'offset' key. the returned 'offset' key
-  // should be equal to 'paging.offset' key 
+  // should be equal to 'paging.offset' key
   // plus 'paging.limit' key (from the API response)
   // hint 1: use the 'URL' object to create an object representation of a url and
   // append the querystring (https://gist.github.com/a0viedo/13241ba70489d6e16805)
   // hint 2: use the fetch API to send a request (https://developers.google.com/web/updates/2015/03/introduction-to-fetch)
   // hint 3: use the utils method to get a string from a querystring object
     exports.search = function(options) {
+
       var qs = {
+        limit: options.limit || '',
+        offset: options.offset || '',
+        price: options.price || '',
+        data: options.data || '',
         official_store_id: 'all'
         //// add other keys here
-      };
+      },
+      url = new URL(ML_SEARCH_URL),
+      response,
+      promise;
 
-      ///// add code here /////
+      url.search = utils.getQueryStr(qs);
+
+      promise = new Promise(function(resolve, reject){
+
+        fetch(url.toString())
+          .then(
+            function(response) {
+              if (response.status !== 200) {
+                reject(Error('the response status was:' + response.status));
+                return;
+              }
+
+              // Examine the text in the response
+              response.json().then(function(data) {
+                var result = {
+                  results: data.results,
+                  next: {
+                    data: qs.data,
+                    limit: data.paging.limit,
+                    offset: data.paging.offset + data.paging.limit,
+                    price: qs.price
+                  }
+                }
+
+                resolve(result);
+              });
+            }
+          )
+          .catch(function(err) {
+            reject(err);
+          });
+
+      });
+
+      return promise;
     };
 });
